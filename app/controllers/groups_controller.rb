8110cc34dfc6b62561fad8773657c25c20ffc437
熊亮 <xiongliang@xiongliangdeMacBook-Pro.local>
2017-05-10 16:53:37 +0800

    create groups index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,7 +30,14 @@ class GroupsController < ApplicationController
      redirect_to groups_path, notice: "Update Success"
    end
 
-private
+    def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      flash[:alert] = "Group deleted"
+      redirect_to groups_path
+    end
+    
+   private
 
   def group_params
     params.require(:group).permit(:title, :description)
